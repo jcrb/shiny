95d17f308de45a6af5c4aebe1f7e6dead836caab
bart-rescue <bart-rescue@localhost.localdomain>
2013-11-07 15:16:26 -0500

    app1

---
+++ shinyapp/ui.R
@@ -0,0 +1,12 @@
+library(shiny)
+
+# Define UI for miles per gallon application
+shinyUI(pageWithSidebar(
+  
+  # Application title
+  headerPanel("Miles par Gallon"),
+  
+  sidebarPanel(),
+  
+  mainPanel()
+))
