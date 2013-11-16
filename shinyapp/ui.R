93e8fa2e09bbeb472e4ec8a9ea4bc7f5a4d9d10c
jcrb <jcb@mint>
2013-11-17 00:29:05 +0100

    suite

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -1,12 +1,41 @@
 library(shiny)
 
-# Define UI for miles per gallon application
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Miles par Gallon"),
-  
-  sidebarPanel(),
-  
-  mainPanel()
-))
+# exercice 1
+# ----------
+# # Define UI for miles per gallon application
+# shinyUI(pageWithSidebar(
+#   
+#   # titre de l'application
+#   headerPanel("Nombre de miles par Gallon"),
+# 
+#   sidebarPanel(),
+#   
+#   mainPanel()
+# ))
+
+# exercice 2
+# ----------
+  # Define UI for miles per gallon application
+  shinyUI(pageWithSidebar(
+    
+    # titre de l'application
+    headerPanel("Nombre de miles par Gallon"),
+    
+    # Sidebar avec des controles pour sélectionner les variables a afficher contre mpg
+    # et pour spécifier si les valeurs aberrantes doivent être incluses
+    sidebarPanel(
+      selectInput("variable", "Variable:",
+                  list("Cylindres" = "cyl", 
+                       "Transmission" = "am", 
+                       "Engrenages" = "gear")),
+      
+      checkboxInput("outliers", "Données aberrantes", FALSE)
+    ),
+    
+    # Show the caption and plot of the requested variable against mpg
+    mainPanel(
+      h3(textOutput("caption")),
+      
+      plotOutput("mpgPlot")
+    )
+  ))
